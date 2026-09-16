658577b4f9211ca381b65c644de0d97230f96415
Paluss1122-1 <paluss1122@gmail.com>
2026-09-17 00:42:39 +0200

    remove depracted models & replace them with new ones

--- app/src/main/java/com/tabslify/tabs/aitab/Models.kt
+++ app/src/main/java/com/tabslify/tabs/aitab/Models.kt
@@ -9,18 +9,19 @@ data class Model(
 )
 
 val geminiModels = listOf(
-    Model("gemini-3.1-flash-lite", vision = true, audio = true, weight = 3),
-    Model("gemini-3-flash-preview", vision = true, audio = true, weight = 3),
-    Model("gemini-2.5-flash", vision = true, audio = true, weight = 3),
-    Model("gemini-2.5-flash-lite", vision = true, audio = true, weight = 1)
+    Model("gemini-3.8-flash", vision = true, audio = true, weight = 3),
+    Model("gemini-3.7-flash", vision = true, audio = true, weight = 3),
+    Model("gemini-3.6-flash", vision = true, audio = true, weight = 2),
+    Model("gemini-3.1-flash-lite", vision = true, audio = true, weight = 2),
+    Model("gemini-3-flash-preview", vision = true, audio = true, weight = 1),
+    Model("gemini-2.5-flash", vision = true, audio = true, weight = 1),
+    Model("gemini-3.5-flash-lite", vision = true, audio = true, weight = 1)
 )
 
 val nvidiaModels = listOf(
     Model("openai/gpt-oss-20b", weight = 1),
-    Model("openai/gpt-oss-120b", weight = 2),
-    Model("nvidia/nemotron-3-nano-30b-a3b", weight = 2),
-    Model("z-ai/glm-5.2", weight = 5),
-    Model("meta/llama-3.2-90b-vision-instruct", vision = true, weight = 2),
+    Model("z-ai/glm-5-3", weight = 5),
+    Model("meta/llama-3.2-11b-vision-instruct", vision = true, weight = 2),
     Model("nvidia/nemotron-3-nano-omni-30b-a3b-reasoning", vision = true, weight = 2)
 )
 
