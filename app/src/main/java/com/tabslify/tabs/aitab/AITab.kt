ee55389f02f626fa118ef6cfc5629d123173c344
Paluss1122-1 <paluss1122@gmail.com>
2026-09-17 00:42:22 +0200

    add navigationBarsPadding

--- app/src/main/java/com/tabslify/tabs/aitab/AITab.kt
+++ app/src/main/java/com/tabslify/tabs/aitab/AITab.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -247,6 +248,7 @@ fun AITabContent(
         Modifier
             .fillMaxSize()
             .imePadding()
+            .navigationBarsPadding()
             .alpha(alpha.value)
     ) {
         Box(
